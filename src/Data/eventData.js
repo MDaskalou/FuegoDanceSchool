46496a03ddb93bae769619e9b4f1f0e948ce0372
Mikael Daskalou <mikael.daskalou@hotmail.com>
2025-10-18 18:19:15 +0200

    Lagt till event

--- src/Data/eventData.js
+++ src/Data/eventData.js
@@ -15,15 +15,28 @@ const events = [
 
     {
         id: 2,
-        date: "23 August 2025",
-        time: "12:00–15:00",
-        location: "Fuego Studio, Göteborg",
+        date: "8 November 2025",
+        time: "20:00–02:00",
+        location: "Södra Allégatan 4, Göteborg",
         levelKey: "events.event2.level",
         bringKey: "events.event2.bring",
         shortDescriptionKey: "events.event2.shortDescription",
         descriptionKey: "events.event2.description",
-        image: require("../img/Events/Nora&SiscoEvent.jpg"),
-        link: "https://app.coursely.se/activity/ea84c52e-ccc0-4938-adca-29212e16c383"
+        image: require("../img/Events/FB Event Cover_8 Nov Fuego Night.png"),
+        link: "https://app.coursely.se/activity/edd709b8-7593-4321-8fdb-1ec84ea81a7a"
+    },
+
+    {
+        id: 3,
+        date: "1 November 2025",
+        time: "12:00–15:00",
+        location: "Doktor Westrings Gata 14D, Göteborg",
+        levelKey: "events.event3.level",
+        bringKey: "events.event3.bring",
+        shortDescriptionKey: "events.event3.shortDescription",
+        descriptionKey: "events.event3.description",
+        image: require("../img/Events/A3 Poster_Beginners intensive course (Facebook-inlägg).png"),
+        link: "https://app.coursely.se/activity/d9828201-9684-49bc-a383-e3b8a004803a"
     }
 ];
 
